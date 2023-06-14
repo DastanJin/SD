5eb6159ad5fa69a8d969521e44611f27af0e56fb
Stanislav Šustr <stanislav.sustr@avesoft.cz>
2023-06-14 21:45:16 +0200

    Style update and half hour update

--- SD/MainWindow.xaml.cs
+++ SD/MainWindow.xaml.cs
@@ -16,18 +16,15 @@ using System.Windows.Shapes;
 
 namespace SD
 {
-  /// <summary>
-  /// Interaction logic for MainWindow.xaml
-  /// </summary>
   public partial class MainWindow : Window
   {
     public MainWindow()
     {
       InitializeComponent();
       this.ResizeMode = ResizeMode.NoResize;
-      Close.IsChecked = Properties.Settings.Default.CloseCheckBox;
+      CloseBtn.IsChecked = Properties.Settings.Default.CloseCheckBox;
     }
-    public void Shutdown(int hours)
+    public void Shutdown(double hours)
     {
       ProcessStartInfo process = new("shutdown", $"/s /t {hours * 3600}")
       {
@@ -35,7 +32,7 @@ namespace SD
         CreateNoWindow = true
       };
       Process.Start(process);
-      if (Close.IsChecked == true) Close();
+      if (CloseBtn.IsChecked == true) Close();
     }
     public void CancelShutdown()
     {
@@ -45,37 +42,52 @@ namespace SD
         CreateNoWindow = true
       };
       Process.Start(process);
-      if (Close.IsChecked == true) Close();
+      if (CloseBtn.IsChecked == true) Close();
     }
 
     private void Plus_Click(object sender, RoutedEventArgs e)
     {
       if (GetHoursValue() >= 24) return;
 
-      Hours.Content = $"{GetHoursValue() + 1} h";
+      Hours.Content = $"{GetHoursValue() + 0.5:0.0} h";
     }
 
 
-    public int GetHoursValue() => int.Parse(s: Hours.Content.ToString().Split(" ").FirstOrDefault());
+    public double GetHoursValue() => double.TryParse(s: Hours.Content.ToString()?.Split(" ").FirstOrDefault() ?? "1", out double d) ? d : 1;
 
     private void Minus_Click(object sender, RoutedEventArgs e)
     {
-      if (GetHoursValue() == 1) return;
+      if (GetHoursValue() == 0.5) return;
 
-      Hours.Content = $"{GetHoursValue() - 1} h";
+      Hours.Content = $"{GetHoursValue() - 0.5:0.0} h";
     }
 
     private void Cancel_Click(object sender, RoutedEventArgs e) => CancelShutdown();
 
     private void Shutdown_Click(object sender, RoutedEventArgs e)
     {
-      int h = GetHoursValue();
+      double h = GetHoursValue();
       Shutdown(h);
     }
 
-    private void Close_Click(object sender, RoutedEventArgs e) {
-      Properties.Settings.Default.CloseCheckBox = (bool)Close.IsChecked;
+    private void Close_Click(object sender, RoutedEventArgs e)
+    {
+      Properties.Settings.Default.CloseCheckBox = CloseBtn.IsChecked == true;
       Properties.Settings.Default.Save();
     }
+    private void Button_MouseEnter(object sender, MouseEventArgs e)
+    {
+      Button button = (Button)sender;
+      button.Background = new SolidColorBrush(Color.FromArgb(100, 100, 20, 20)); // Set a lighter background color
+    }
+
+    private void Button_MouseLeave(object sender, MouseEventArgs e)
+    {
+      Button button = (Button)sender;
+      button.Background = new SolidColorBrush(Color.FromArgb(100, 20, 20, 20)); // Restore the original background color
+    }
+
+    public new void Close() => base.Close();
+
   }
 }
